71dc2ba66d57b4fd0ade393761713f17b17854c6
Great2008 <aakorokaragreatname@gmail.com>
2025-10-15 19:45:52 +0100

    update settings.py

--- reads_mvp/settings.py
+++ reads_mvp/settings.py
@@ -48,15 +48,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'reads_mvp.wsgi.application'
 
 import os
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        conn_max_age=600,
+    )
 }
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
