b100bade0452143f482aa9bb92dc75432b9d5812
Great2008 <aakorokaragreatname@gmail.com>
2025-10-16 20:43:42 +0100

    update views.py

--- learn/views.py
+++ learn/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Lesson
 
+
+@login_required(login_url='/users/login/')
 def home(request):
     lessons = Lesson.objects.all()
     return render(request, 'learn/home.html', {'lessons': lessons})
 
+
+@login_required(login_url='/users/login/')
 def lesson_detail(request, pk):
     lesson = get_object_or_404(Lesson, pk=pk)
     return render(request, 'learn/detail.html', {'lesson': lesson})
-    from django.contrib.auth.decorators import login_required
-
-@login_required(login_url='/users/login/')
-def home(request):
-    ...
