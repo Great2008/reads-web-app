16e838c3dd39db5a9354efac6dbb3a588616e7f7
Great2008 <aakorokaragreatname@gmail.com>
2025-10-19 08:17:47 +0100

    update urls.py

--- reads_mvp/urls.py
+++ reads_mvp/urls.py
@@ -6,16 +6,3 @@ urlpatterns = [
     path('', include('learn.urls')),
     path('users/', include('users.urls')),
 ]
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('learn.urls')),
-]
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('learn.urls')),
-    path('users/', include('users.urls')),
-]
