ff58f03b53b6e247006d42293e129608ee9fd499
Great2008 <aakorokaragreatname@gmail.com>
2025-10-31 16:17:49 +0100

    update

--- quiz/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<int:lesson_id>/', views.take_quiz, name='take_quiz'),
-    path('<int:lesson_id>/submit/', views.submit_quiz, name='submit_quiz'),
-]
